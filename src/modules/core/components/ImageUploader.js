4a95115a2cfafae4a339a103b4f5f651a36a8d49
joshuamshana <joshua.mshana@nmbtz.com>
2023-07-22 21:15:51 +0300

    Add cancel and crop action

--- src/modules/core/components/ImageUploader.js
+++ src/modules/core/components/ImageUploader.js
@@ -14,8 +14,8 @@ const mainStyle = {
 };
 
 const editorStyle = {
-    minWidth: 300,
-    maxWidth: '80%',
+    width: '100vw',
+    marginTop: 54,
     minHeight: 300,
     borderRadius: 8,
     backgroundColor: '#f5f5f5',
@@ -23,11 +23,34 @@ const editorStyle = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'relative'
+    position: 'relative',
 };
 
 const barStyle = {
-    backgroundColor: 'rgba(0,0,0,0.35)'
+    backgroundColor: 'rgba(0,0,0,0.5)'
+}
+
+function CloseIcon({onPress}) {
+    return (
+        <div onClick={onPress} style={{position: "relative", height: 48, width: 48}}>
+            <div style={{
+                position: "absolute",
+                width: 24,
+                height: 5,
+                borderRadius: 8,
+                rotate: '45deg',
+                backgroundColor: 'black'
+            }}/>
+            <div style={{
+                position: "absolute",
+                width: 24,
+                height: 5,
+                borderRadius: 8,
+                rotate: '135deg',
+                backgroundColor: 'black'
+            }}/>
+        </div>
+    )
 }
 
 function LeftBar({size}) {
@@ -72,7 +95,8 @@ export function ImageUploader() {
         right_size: 10,
         bottom_size: 10,
         top_size: 10,
-        target_width: 0
+        target_width: 0,
+        target_height: 0
     });
     const [source, setSource] = useState();
     const [sourceCropped, setSourceCropped] = useState();
@@ -163,6 +187,17 @@ export function ImageUploader() {
         }
     }, [source, mainDivRef]);
 
+    const cancelCrop = useMemo(()=>{
+        return ()=>{
+            setSource(undefined);
+            fileInputRef.current.value=null;
+        }
+    },[]);
+    const cropImage = useMemo(()=>{
+        return ()=>{
+            console.log(JSON.stringify(windowData,null,2))
+        }
+    },[windowData]);
     return (
         <>
             <div onClick={uploadImage} style={mainStyle}>
@@ -189,13 +224,49 @@ export function ImageUploader() {
                         top: 0,
                         right: 0,
                         left: 0,
-                        background: "white",
+                        background: "black",
                         zIndex: 9999999,
                         height: '100vh',
                         display: 'flex',
                         justifyContent: 'center',
-                        alignItems: 'center'
+                        alignItems: 'center',
                     }}>
+                        <div style={{
+                            height: 54,
+                            position: 'fixed',
+                            top: 0,
+                            width: '100%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            flexDirection: 'row'
+                        }}>
+                            <div onClick={cancelCrop} style={{
+                                backgroundColor: 'red',
+                                flexGrow: 1,
+                                height: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                display: 'flex',
+                                cursor: 'pointer'
+                            }}>
+                                <span style={{
+                                    fontSize: 16, fontWeight: 600, color: 'white'
+                                }}>CANCEL</span>
+                            </div>
+                            <div style={{
+                                flexGrow: 1,
+                                height: '100%',
+                                backgroundColor: '#343434',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                display: 'flex',
+                                cursor: 'pointer'
+                            }}>
+                                <span onClick={cropImage} style={{fontSize: 16, fontWeight: 600, color: 'white'}}>
+                                    DONE CROP
+                                </span>
+                            </div>
+                        </div>
                         <div
                             id={'id123'}
                             ref={mainDivRef}
@@ -212,33 +283,21 @@ export function ImageUploader() {
                             }}
                             style={editorStyle}
                         >
-                            {
-                                source && <>
-                                    <img onLoad={_ => {
-                                        setMainDivRef(createRef);
-                                    }} style={{width: '100%'}} alt={''} src={source}/>
-                                    <LeftBar size={windowData.left_size}/>
-                                    <RightBar size={windowData.right_size}/>
-                                    <TopBar
-                                        width={windowData?.target_width}
-                                        marginLeft={windowData.left_size}
-                                        marginRight={windowData.right_size}
-                                        size={windowData.top_size}/>
-                                    <BottomBar
-                                        width={windowData.target_width}
-                                        marginLeft={windowData.left_size}
-                                        marginRight={windowData.right_size}
-                                        size={windowData.bottom_size}/>
-                                </>
-                            }
-                            <input
-                                multiple={false}
-                                onChange={handleImage}
-                                accept={'image/*'}
-                                type={'file'}
-                                ref={fileInputRef}
-                                style={{display: 'none'}}
-                            />
+                            <img onLoad={_ => {
+                                setMainDivRef(createRef);
+                            }} style={{width: '100%'}} alt={''} src={source}/>
+                            <LeftBar size={windowData.left_size}/>
+                            <RightBar size={windowData.right_size}/>
+                            <TopBar
+                                width={windowData?.target_width}
+                                marginLeft={windowData.left_size}
+                                marginRight={windowData.right_size}
+                                size={windowData.top_size}/>
+                            <BottomBar
+                                width={windowData.target_width}
+                                marginLeft={windowData.left_size}
+                                marginRight={windowData.right_size}
+                                size={windowData.bottom_size}/>
                         </div>
                     </div>
                 </>
